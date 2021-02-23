04b51ed50187b39766d32a73d8b95c6ee09f0cc7
dawoud-os <dawoud@opensolar.com>
2021-02-23 15:17:45 +1100

    fix: new test

--- opensolar_ci/dummyapp/tests.py
+++ opensolar_ci/dummyapp/tests.py
@@ -5,3 +5,6 @@ from django.test import TestCase
 class DummyTestCase(TestCase):
     def test_dummy(self):
         self.assertTrue(1)
+
+    def test_another_dummy(self):
+        self.assertTrue(1)
